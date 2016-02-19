3aa31ec7c58b705b4ff5f5100f2c604c23d6b733
dquilter <david_quilter@hotmail.com>
2016-02-19 16:53:01 +0000

    'Init'

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    livereload: true,
+                    open: true
+                }
+            }
+        },
+        bake: {
+            dohtml: {
+                options: {},
+                files: {
+                    'dist/index.html': 'src/html/pages/index.html'
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: {
+                    'dist/css/styles.css': 'src/sass/styles.scss',
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['src/sass/**/*.scss'],
+                tasks: 'sass',
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: ['*.html'],
+                options: {
+                    livereload: true
+                }
+            },
+            bake: {
+                files: ['src/html/pages/src/**', 'src/html/includes/**'],
+                tasks: 'bake', 
+                options: {
+                    livereload: true
+                }        
+            },
+            js: {
+                files: ['src/js/*.js'],
+                options: {
+                    livereload: true
+                }        
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-bake');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    
+    grunt.registerTask('default', ['bake', 'sass', 'connect', 'watch']);
+    
+};
